6e4a91957aaa17a898a08e0f440d286af8643d19
akshat3358 <akshatchaturvedi422@gmail.com>
2020-09-30 13:52:42 +0530

    new change in dev branch

--- p2.py
+++ p2.py
@@ -1,4 +1,4 @@
 print("Hello world")
 print("going to add some features")
 print("added print")
-print("print feature added in p2.py file in dev branch")
+print("new print feature added in p2.py file in dev branch")
